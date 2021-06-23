d972c1e8c547068245e04f9335856bcbb977243b
Ryan Weland <rweland@cazireth.com>
2021-06-22 20:14:28 -0400

    Added ability to use symlink to temp dir

--- src/TTSAdapter.ts
+++ src/TTSAdapter.ts
@@ -20,7 +20,7 @@ import { resolveModule } from 'luabundle/bundle/process';
 import { readFileSync, readlinkSync } from 'fs';
 
 import parse from './bbcode/tabletop';
-import { tempFolder, docsFolder, FileHandler, isTempFolder } from './filehandler';
+import { tempFolder, docsFolder, FileHandler } from './filehandler';
 
 /**
  * Shape of data received from TTS
@@ -146,12 +146,26 @@ export default class TTSAdapter {
     this.server.listen(port, 'localhost'); // Open Server
   }
 
+  private isTempFolder(dir: Uri) {
+    if (dir.uri.fsPath === this.tempUri.fsPath) {
+      return true;
+    }
+
+    try {
+      let resolvedSymlink = readlinkSync(dir)
+      return resolvedSymlink === this.tempUri.fsPath;
+    }
+    catch (reason: any) {
+      return false;
+    }
+  }
+
   /**
    * Retrieves scripts from currently open savegame
    */
   public async getScripts() {
     const vsFolders = ws.workspaceFolders;
-    if (!vsFolders || vsFolders.findIndex((dir) => isTempFolder(dir.uri.fsPath)) {
+    if (!vsFolders || vsFolders.findIndex((dir) => this.isTempFolder(dir)) === -1) {
       ws.updateWorkspaceFolders(vsFolders ? vsFolders.length : 0, null, { uri: this.tempUri });
     }
     TTSAdapter.sendToTTS(TxMsgType.GetScripts);
